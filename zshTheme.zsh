b6dd3291cdc8153b53869ba9ca02ad62ea468608
caillouc <cailloucolson@gmail.com>
2020-05-06 12:55:43 +0200

    change color of name if command failed

--- zshTheme.zsh
+++ zshTheme.zsh
@@ -1,7 +1,7 @@
 # add "source PATH/TO/THIS/FILE" in ~/.oh-my-zsh/custom/themes/name_of_your_theme.zsh-theme
 
-PROMPT="%(?:%{$fg_bold[green]%}➜ :%{$fg_bold[red]%}➜ )"
-PROMPT+='%{$fg[green]%}%n %{$fg_bold[blue]%}%2~ $(git_prompt_info)%{$reset_color%}%(!.#.$) '
+PROMPT="%(?:%{$fg_bold[green]%}%n :%{$fg_bold[red]%}%n )"
+PROMPT+='%{$fg_bold[blue]%}%2~ $(git_prompt_info)%{$reset_color%}%(!.#.$) '
 
 ZSH_THEME_GIT_PROMPT_PREFIX="%{$fg[red]%}‹"
 ZSH_THEME_GIT_PROMPT_SUFFIX="› %{$reset_color%}"
